470ee2b53428afdffb1c45ada2825941ef7ae170
Kostas Chartsias <KostasChar@users.noreply.github.com>
2016-11-11 14:53:45 +0200

    Update test1.rb

--- test1.rb
+++ test1.rb
@@ -1,16 +1,15 @@
+#needs mutex because of unpredictable context switching 
+
 threads = []
 lock=Mutex.new
-
 threads << Thread.new do
-	lock.synchronize{
-	100.times { puts "This is SPARTA" } }
-	sleep(10)
+    lock.synchronize{
+    100.times { puts "This is SPARTA" } }
+    sleep(10)
 end
-
 threads << Thread.new do
-	lock.synchronize{
+    lock.synchronize{
     100.times { puts "THIS IS ABSURD" } }
     sleep(10)
 end
-
 threads.each { |thr| thr.join }
